8772b8d4c9d4a8629e83382d3b30e4fe06661fea
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-31 17:51:10 -0400

    HTML created in JS

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// Array that holds the user command-line arguments
+const profileDataArgs = process.argv.slice(2);
+const [name,github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require("./src/page-template");
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+// First argument name of file, second data written and third callback error handling 
+fs.writeFile(`./index.html`, generatePage(name,github), err =>{
+    if(err) throw new Error (err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
