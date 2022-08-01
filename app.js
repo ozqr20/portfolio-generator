f37d4d22cdf2cef8270b9bab5ec4084378717897
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-31 23:17:42 -0400

    packages

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// Array that holds the user command-line arguments
-const profileDataArgs = process.argv.slice(2);
-const [name,github] = profileDataArgs;
-const fs = require('fs');
-const generatePage = require("./src/page-template");
+const inquirer = require('inquirer');
 
-// First argument name of file, second data written and third callback error handling 
-fs.writeFile(`./index.html`, generatePage(name,github), err =>{
-    if(err) throw new Error (err);
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// Array that holds the user command-line arguments
+// const profileDataArgs = process.argv.slice(2);
+// const [name,github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require("./src/page-template");
+// const pageHTML = generatePage(name,github);
+
+// // First argument name of file, second data written and third callback error handling 
+// fs.writeFile(`./index.html`, generatePage(name,github), err =>{
+//     if(err) throw new Error (err);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
