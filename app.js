b2008934a3be602c1ab16739017f2cde02546127
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-08-05 12:45:23 -0400

    modifying the HTML template with styling

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require("./src/page-template");
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -131,14 +134,17 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+        console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
 // Array that holds the user command-line arguments
 // const profileDataArgs = process.argv.slice(2);
 // const [name,github] = profileDataArgs;
-// const fs = require('fs');
-// const generatePage = require("./src/page-template");
+
 // const pageHTML = generatePage(name,github);
 
 // // First argument name of file, second data written and third callback error handling 
@@ -146,3 +152,4 @@ promptUser()
 //     if(err) throw new Error (err);
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+
